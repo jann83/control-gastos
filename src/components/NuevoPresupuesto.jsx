711b8c501669b9776fabdda28630761a80ab089d
jann83 <83481345+jann83@users.noreply.github.com>
2023-07-19 17:17:48 -0600

    validacion presupuesto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,28 +1,28 @@
 import { useState } from "react";
 import Mensaje from "./Mensaje";
 
+const NuevoPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto
+}) => {
+  const [mensaje, setMensaje] = useState("");
 
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje("No es un presupuesto válido");
+      return;
+    }
+    setMensaje("")
+    setIsValidPresupuesto(true)
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
-
-const [mensaje, setMensaje] = useState ('');
-
-
-const handlePresupuesto= (e) =>{
-
-e.preventDefault();
-if(!presupuesto  || presupuesto < 0 ){
-    setMensaje('No es un presupuesto válido')
-return
-}
-setMensaje('')
-
-}
+  };
 
   return (
     <div className="contenedor-´resupuesto contenedor sombra  ">
-      <form onSubmit={handlePresupuesto} 
-      action=" " className="formulario ">
+      <form onSubmit={handlePresupuesto} action=" " className="formulario ">
         <div className="campo">
           <label htmlFor="">Definir Presupuesto </label>
           <input
@@ -30,15 +30,12 @@ setMensaje('')
             type="number"
             placeholder="Añade tu Presupuesto"
             value={presupuesto}
-            onChange={e =>setPresupuesto(Number(e.target.value))}
-
-
-
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
           />
         </div>
 
         <input type="submit" value="Añadir" />
-        {mensaje && <Mensaje tipo='error'>{mensaje} </Mensaje>   }
+        {mensaje && <Mensaje tipo="error">{mensaje} </Mensaje>}
       </form>
     </div>
   );
